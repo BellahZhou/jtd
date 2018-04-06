36c8058b6669eac8aba8b8da705cadcd1a79ba24
Bellah <18351886003@163.com>
2018-04-06 14:25:07 +0800

    Back--up top

---
+++ src/main/java/com/jtd/service/LyService.java
@@ -0,0 +1,11 @@
+package com.jtd.service;
+
+import java.util.List;
+
+import com.jtd.entity.Ly;
+
+public interface LyService {
+	public List<Ly> getLysByUserId(Long userId);
+
+	public int insert(Ly ly);
+}
---
+++ src/main/java/com/jtd/service/impl/LyServiceImpl.java
@@ -0,0 +1,27 @@
+package com.jtd.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.jtd.dao.LyDao;
+import com.jtd.entity.Ly;
+import com.jtd.service.LyService;
+@Service
+public class LyServiceImpl implements LyService{
+	@Resource
+	private LyDao lyDao;
+	
+	@Override
+	public List<Ly> getLysByUserId(Long userId) {
+		return lyDao.getLysByUserId(userId);
+	}
+
+	@Override
+	public int insert(Ly ly) {
+		return lyDao.insert(ly);
+	}
+
+}
